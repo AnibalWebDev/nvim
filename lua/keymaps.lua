05755e1b304502130dd21b144f47150bbd3e27ad
AnibalWebDev <thesmartweirdohax@protonmail.com>
2025-03-30 19:34:04 -0500

    Cleaned up keybindings
    - corrected keybindings that conflicted w/ editor or plugin defaults
    - added comments on all keybindings
    - improved groupings by plugin or related functionality

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,89 +2,63 @@ local function map(m, k, v)
 	vim.keymap.set(m, k, v)
 end
 
--- Misc keymaps
+-- Save & Exit
 
-map("n", "<A-n>", ":tabnew<CR>")
-map("n", "<A-q>", ":q<CR>")
-map("n", "<A-s>", ":w<CR>")
-map("n", "<A-w>", ":wq<CR>")
-map("n", "<A-%>", ":source %<CR>")
-map("n", "<C-C>", ":e ~/.config/nvim/init.lua<CR>")
+map("n", "<A-q>", ":q<CR>")      -- exit neovim w/o saving
+map("n", "<A-n>", ":tabnew<CR>") -- new tab
+map("n", "<A-s>", ":w<CR>")      -- save file
+map("n", "<A-w>", ":wq<CR>")     -- save and quit
 
--- Surround keymaps
---[[
-map("n", "<C-q>", "i\"<ESC>ea\"<ESC>")
-map("n", "<C-w>", "i'<ESC>ea'<ESC>")
-map("n", "<C-e>", "i(<ESC>ea)<ESC>")
-map("n", "<C-r>", "i[<ESC>ea]<ESC>")
-map("n", "<C-t>", "i{<ESC>ea}<ESC>")
-map("n", "<C-y>", "i<<ESC>ea><ESC>")
-map("n", "<C-u>", "i{% <ESC>ea %}<ESC>")
-map("n", "<C-i>", "i{{ <ESC>ea }}<ESC>")
-]]
+-- Misc
 
--- Move window keymaps
+map("n", "<C-n>", ":Neotree toggle<CR>")    -- toggle NeoTree file explorer
+map("n", "<A-x>", ":CopilotChatToggle<CR>") -- toggle Copilot Chat
+map("n", "<A-L>", ":Lazy<CR>")              -- open Lazy package manager
+map("n", "<A-%>", ":source %<CR>")          -- source file in current buffer
+map("n", "<A-b>", ":bd<CR>")                -- kill the current buffer
 
-map("n", "<C-h>", "<C-w>H")
-map("n", "<C-j>", "<C-w>J")
-map("n", "<C-k>", "<C-w>K")
-map("n", "<C-l>", "<C-w>L")
+-- Word Surrounds
 
--- Resize window keymaps
+map("n", "<A-a>", "i\"<ESC>ea\"<ESC>")   -- surround word w/ ""
+map("n", "<A-c>", "i'<ESC>ea'<ESC>")     -- surround word w/ ''
+map("n", "<A-e>", "i(<ESC>ea)<ESC>")     -- surround word w/ ()
+map("n", "<A-r>", "i[<ESC>ea]<ESC>")     -- surround word w/ []
+map("n", "<A-o>", "i{<ESC>ea}<ESC>")     -- surround word w/ {}
+map("n", "<A-y>", "i<<ESC>ea><ESC>")     -- surround word w/ <>
+map("n", "<A-u>", "i{% <ESC>ea %}<ESC>") -- surround word w/ (% %)
+map("n", "<A-i>", "i{{ <ESC>ea }}<ESC>") -- surround word w/ {{  }}
 
-map("n", "<A-j>", "<C-w>+5")
-map("n", "<A-k>", "<C-w>-5")
-map("n", "<A-h>", "<C-w><5")
-map("n", "<A-l>", "<C-w>>5")
+-- Windows
 
--- NeoTree keymaps
+map("n", "<C-p>", "<C-w>n")  -- new window (horizontal split)
 
-map("n", "<C-n>", ":Neotree toggle<CR>")
+map("n", "<C-h>", "<C-w>H")  -- move window left
+map("n", "<C-l>", "<C-w>L")  -- move window right
+map("n", "<C-k>", "<C-w>K")  -- move window up
+map("n", "<C-j>", "<C-w>J")  -- move window down
 
--- Lazy keymaps
+map("n", "<A-h>", "<C-w><5") -- adjust window width left 5 cols
+map("n", "<A-l>", "<C-w>>5") -- adjust window width right 5 cols
+map("n", "<A-k>", "<C-w>-5") -- adjust window width up 5 cols
+map("n", "<A-j>", "<C-w>+5") -- adjust window width down 5 cols
 
-map("n", "<A-L>", ":Lazy<CR>")
+-- LSP
 
--- Buffer keymaps
+map("n", "<A-d>", ":lua vim.lsp.buf.definition()<CR>") -- LSP jump to definition of an identifier
+map("n", "<A-;>", ":lua vim.lsp.buf.rename()<CR>")     -- LSP rename an identifier
+map("n", "K", ":lua vim.lsp.buf.hover()<CR>")          -- LSP hover docstring for an identifier
 
-map("n", "<C-K>", ":bd<CR>")
+-- Telescope
 
--- Luline keymaps
-
-map("n", "<A-0>", ":LualineToggle<CR>")
-map("n", "<A-1>", ":LualineBuffersJump 1<CR>")
-map("n", "<A-2>", ":LualineBuffersJump 2<CR>")
-map("n", "<A-3>", ":LualineBuffersJump 3<CR>")
-map("n", "<A-4>", ":LualineBuffersJump 4<CR>")
-map("n", "<A-5>", ":LualineBuffersJump 5<CR>")
-map("n", "<A-6>", ":LualineBuffersJump 6<CR>")
-map("n", "<A-7>", ":LualineBuffersJump 7<CR>")
-map("n", "<A-8>", ":LualineBuffersJump 8<CR>")
-map("n", "<A-9>", ":LualineBuffersJump 9<CR>")
-
--- Copilot keymaps
-
-map("n", "<A-x>", ":Copilot toggle<CR>")
-map("n", "<A-]>", ":Copilot next<CR>")
-map("n", "<A-[>", ":Copilot prev<CR>")
-
--- LSP keymaps
-
-map("n", "<A-d>", ":lua vim.lsp.buf.definition()<CR>")
-map("n", "<A-;>", ":lua vim.lsp.buf.rename()<CR>")
-map("n", "K", ":lua vim.lsp.buf.hover()<CR>")
-
--- Telescope keymaps
-
-map("n", "<A-F>", ":Telescope find_files<CR>")
-map("n", "<A-G>", ":Telescope live_grep<CR>")
-map("n", "<A-B>", ":Telescope buffers<CR>")
-map("n", "<A-H>", ":Telescope help_tags<CR>")
-map("n", "<A-M>", ":Telescope marks<CR>")
-map("n", "<A-R>", ":Telescope registers<CR>")
-map("n", "<A-S>", ":Telescope lsp_document_symbols<CR>")
-map("n", "<A-T>", ":Telescope treesitter<CR>")
-map("n", "<A-V>", ":Telescope vim_options<CR>")
-map("n", "<A-X>", ":Telescope commands<CR>")
-map("n", "<A-Z>", ":Telescope colorscheme<CR>")
-map("n", "<A-~>", ":Telescope lsp_workspace_symbols<CR>")
+map("n", "<A-F>", ":Telescope find_files<CR>")            -- find files
+map("n", "<A-G>", ":Telescope live_grep<CR>")             -- live grep
+map("n", "<A-B>", ":Telescope buffers<CR>")               -- open buffers
+map("n", "<A-H>", ":Telescope help_tags<CR>")             -- help docs
+map("n", "<A-M>", ":Telescope marks<CR>")                 -- marks
+map("n", "<A-R>", ":Telescope registers<CR>")             -- registers
+map("n", "<A-S>", ":Telescope lsp_document_symbols<CR>")  -- document symbols
+map("n", "<A-T>", ":Telescope treesitter<CR>")            -- treesitter symbols
+map("n", "<A-V>", ":Telescope vim_options<CR>")           -- vim options
+map("n", "<A-X>", ":Telescope commands<CR>")              -- vim commands
+map("n", "<A-Z>", ":Telescope colorscheme<CR>")           -- colorschemes
+map("n", "<A-~>", ":Telescope lsp_workspace_symbols<CR>") -- LSP workspace symbols
