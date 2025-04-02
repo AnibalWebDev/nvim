9a07d4f997993a47b680b3bf8382489e3df44fc6
AnibalWebDev <thesmartweirdohax@protonmail.com>
2025-04-01 20:25:33 -0500

    toggleterm float options deleted and is now displayed in tab

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,18 +3,8 @@ return {
 		'akinsho/toggleterm.nvim',
 		config = function()
 			require("toggleterm").setup({
-				size = 12,
 				open_mapping = [[<A-t>]],
-				direction = "float",
-				float_opts = {
-					border = "curved",
-					width = 75,
-					winblend = 0,
-					highlights = {
-						border = "Normal",
-						background = "Normal",
-					},
-				},
+				direction = "tab",
 				close_on_exit = true,
 				shell = vim.o.shell,
 			})
