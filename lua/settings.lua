051551bc05ec5697fdae3a7c066c01633468b7bf
AnibalWebDev <thesmartweirdohax@protonmail.com>
2025-04-06 08:49:03 -0500

    No line wrap in editor

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,18 @@
 vim.o.number = true
 vim.o.relativenumber = true
+
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smarttab = true
 vim.o.autoindent = true
+
 vim.o.mouse = ""
 vim.o.hlsearch = false
 vim.o.background = "dark"
 vim.o.termguicolors = true
 vim.o.showcmd = false
 vim.o.wrap = true
+vim.o.wrap = false
 vim.o.columns = 100
 
 vim.g.mapleader = " "
