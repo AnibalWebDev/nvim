d2027cd7ca1451f6b2d5d88264b652fcb081dcfd
AnibalWebDev <thesmartweirdohax@protonmail.com>
2025-04-06 08:48:34 -0500

    Make lua_ls aware of nvim runtime files for autocompletion and hover docs

--- lua/plugins/cmp-nvim-lsp.lua
+++ lua/plugins/cmp-nvim-lsp.lua
@@ -55,7 +55,29 @@ return {
 			}
 
 			require('lspconfig').lua_ls.setup {
-				capabilities = capabilities,
+				cmd = { "lua-language-server", "--stdio" },
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							version = 'LuaJIT',
+							-- Setup your Neovim runtime path
+							path = vim.split(package.path, ';'),
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { 'vim' },
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
 			}
 
 			require('lspconfig').ts_ls.setup {
